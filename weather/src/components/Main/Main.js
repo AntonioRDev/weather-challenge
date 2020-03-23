20bfa5c26fe0b36eea5db164a04b8d29ba8925fb
Antônio Carlos <antonio@beestart.com.br>
2020-03-23 14:32:47 -0300

    Min e Max da tela inicial

--- weather/src/components/Main/Main.js
+++ weather/src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Loader from "react-loader-spinner";
 import { getCityWeather } from "../../services/api";
 
 import InputText from "../InputText/InputText";
@@ -8,82 +9,194 @@ import HorizontalLine from "../HorizontalLine/HorizontalLine";
 import CardDetail from "../CardDetail/CardDetail";
 
 export default function Main() {
-  const onSearch = cityName => {
-    getCityWeather(cityName)
-      .then(data => console.log("data", data))
-      .catch(error => console.log(error));
+  const [RJ, setRJ] = useState({});
+  const [SP, setSP] = useState({});
+  const [BH, setBH] = useState({});
+  const [DF, setDF] = useState({});
+  const [BL, setBL] = useState({});
+  const [SV, setSV] = useState({});
+  const [CR, setCR] = useState({});
+  const [FL, setFL] = useState({});
+  const [MN, setMN] = useState({});
+  const [JP, setJP] = useState({});
+
+  const [loading, setLoading] = useState(false);
+
+  const onSearch = async cityName => {
+    try {
+      const response = await getCityWeather(cityName);
+      console.log(response.data);
+    } catch (error) {
+      console.log("onSearch error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+
+      const responseRJ = await getCityWeather("Rio de Janeiro");
+      const rj = {
+        min: responseRJ.data.data[0].low_temp,
+        max: responseRJ.data.data[0].max_temp
+      };
+      setRJ(rj);
+
+      const responseSP = await getCityWeather("São Paulo");
+      const sp = {
+        min: responseSP.data.data[0].low_temp,
+        max: responseSP.data.data[0].max_temp
+      };
+      setSP(sp);
+
+      const responseBH = await getCityWeather("Belo Horizonte");
+      const bh = {
+        min: responseBH.data.data[0].low_temp,
+        max: responseBH.data.data[0].max_temp
+      };
+      setBH(bh);
+
+      const responseDF = await getCityWeather("Brasília");
+      const df = {
+        min: responseDF.data.data[0].low_temp,
+        max: responseDF.data.data[0].max_temp
+      };
+      setDF(df);
+
+      const responseBL = await getCityWeather("Belém");
+      const bl = {
+        min: responseBL.data.data[0].low_temp,
+        max: responseBL.data.data[0].max_temp
+      };
+      setBL(bl);
+
+      const responseSV = await getCityWeather("Salvador");
+      const sv = {
+        min: responseSV.data.data[0].low_temp,
+        max: responseSV.data.data[0].max_temp
+      };
+      setSV(sv);
+
+      const responseCR = await getCityWeather("Curitiba");
+      const cr = {
+        min: responseCR.data.data[0].low_temp,
+        max: responseCR.data.data[0].max_temp
+      };
+      setCR(cr);
+
+      const responseFL = await getCityWeather("Fortaleza");
+      const fl = {
+        min: responseFL.data.data[0].low_temp,
+        max: responseFL.data.data[0].max_temp
+      };
+      setFL(fl);
+
+      const responseMN = await getCityWeather("Manaus");
+      const mn = {
+        min: responseMN.data.data[0].low_temp,
+        max: responseMN.data.data[0].max_temp
+      };
+      setMN(mn);
+
+      const responseJP = await getCityWeather("João Pessoa");
+      const jp = {
+        min: responseJP.data.data[0].low_temp,
+        max: responseJP.data.data[0].max_temp
+      };
+      setJP(jp);
+
+      setLoading(false);
+    } catch (error) {
+      console.log("fetchData", error);
+    }
   };
 
   return (
     <div className="main-container">
-      <div className="main-div">
-        <div className="main-search">
-          <label className="main-lbl">Previsão do Tempo</label>
+      {loading ? (
+        <div className="d-flex align-items-center">
+          <Loader
+            type="Puff"
+            color="#FFF"
+            height={100}
+            width={100}
+            //  timeout={3000} //3 secs
+          />
+        </div>
+      ) : (
+        <div className="main-div">
+          <div className="main-search">
+            <label className="main-lbl">Previsão do Tempo</label>
 
-          {/* <div>
+            {/* <div>
             <CardDetail />
           </div> */}
 
-          <InputText onSearch={searchValue => onSearch(searchValue)} />
-        </div>
+            <InputText onSearch={searchValue => onSearch(searchValue)} />
+          </div>
 
-        <div className="main-hr">
-          <HorizontalLine />
-        </div>
+          <div className="main-hr">
+            <HorizontalLine />
+          </div>
 
-        <div className="pt-5 pl-5">
-          <label className="main-lbl-1">Capitais</label>
+          <div className="pt-5 pl-5">
+            <label className="main-lbl-1">Capitais</label>
 
-          <div className="row mb-3">
-            <div className="col-sm-1">Min</div>
-            <div className="col-sm-1">Máx</div>
-            <div className="col-sm-4"></div>
-            <div className="col-sm-1">Min</div>
-            <div className="col-sm-1">Máx</div>
-            <div className="col-sm-4"></div>
-          </div>
-          <div className="row mb-3">
-            <div className="col-sm-1 main-bold">18°</div>
-            <div className="col-sm-1 main-bold">27°</div>
-            <div className="col-sm-4 main-bold">Rio de Janeiro</div>
-            <div className="col-sm-1 main-bold">23°</div>
-            <div className="col-sm-1 main-bold">37°</div>
-            <div className="col-sm-4 main-bold">Salvador</div>
-          </div>
-          <div className="row mb-3">
-            <div className="col-sm-1 main-bold">14°</div>
-            <div className="col-sm-1 main-bold">22°</div>
-            <div className="col-sm-4 main-bold">São Paulo</div>
-            <div className="col-sm-1 main-bold">5°</div>
-            <div className="col-sm-1 main-bold">14°</div>
-            <div className="col-sm-4 main-bold">Curitiba</div>
-          </div>
-          <div className="row mb-3">
-            <div className="col-sm-1 main-bold">21°</div>
-            <div className="col-sm-1 main-bold">32°</div>
-            <div className="col-sm-4 main-bold">Belo Horizonte</div>
-            <div className="col-sm-1 main-bold">21°</div>
-            <div className="col-sm-1 main-bold">32°</div>
-            <div className="col-sm-4 main-bold">Fortaleza</div>
-          </div>
-          <div className="row mb-3">
-            <div className="col-sm-1 main-bold">24°</div>
-            <div className="col-sm-1 main-bold">37°</div>
-            <div className="col-sm-4 main-bold">Brasília</div>
-            <div className="col-sm-1 main-bold">21°</div>
-            <div className="col-sm-1 main-bold">32°</div>
-            <div className="col-sm-4 main-bold">Manaus</div>
-          </div>
-          <div className="row">
-            <div className="col-sm-1 main-bold">24°</div>
-            <div className="col-sm-1 main-bold">37°</div>
-            <div className="col-sm-4 main-bold">Belém</div>
-            <div className="col-sm-1 main-bold">28°</div>
-            <div className="col-sm-1 main-bold">40°</div>
-            <div className="col-sm-4 main-bold">João Pessoa</div>
+            <div className="row mb-3">
+              <div className="col-sm-1">Min</div>
+              <div className="col-sm-1">Máx</div>
+              <div className="col-sm-4"></div>
+              <div className="col-sm-1">Min</div>
+              <div className="col-sm-1">Máx</div>
+              <div className="col-sm-4"></div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-sm-1 main-bold">{RJ.min}°</div>
+              <div className="col-sm-1 main-bold">{RJ.max}°</div>
+              <div className="col-sm-4 main-bold">Rio de Janeiro</div>
+              <div className="col-sm-1 main-bold">{SV.min}°</div>
+              <div className="col-sm-1 main-bold">{SV.max}°</div>
+              <div className="col-sm-4 main-bold">Salvador</div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-sm-1 main-bold">{SP.min}°</div>
+              <div className="col-sm-1 main-bold">{SP.max}°</div>
+              <div className="col-sm-4 main-bold">São Paulo</div>
+              <div className="col-sm-1 main-bold">{CR.min}°</div>
+              <div className="col-sm-1 main-bold">{CR.max}°</div>
+              <div className="col-sm-4 main-bold">Curitiba</div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-sm-1 main-bold">{BH.min}°</div>
+              <div className="col-sm-1 main-bold">{BH.max}°</div>
+              <div className="col-sm-4 main-bold">Belo Horizonte</div>
+              <div className="col-sm-1 main-bold">{FL.min}°</div>
+              <div className="col-sm-1 main-bold">{FL.max}°</div>
+              <div className="col-sm-4 main-bold">Fortaleza</div>
+            </div>
+            <div className="row mb-3">
+              <div className="col-sm-1 main-bold">{DF.min}°</div>
+              <div className="col-sm-1 main-bold">{DF.max}°</div>
+              <div className="col-sm-4 main-bold">Brasília</div>
+              <div className="col-sm-1 main-bold">{MN.min}°</div>
+              <div className="col-sm-1 main-bold">{MN.max}°</div>
+              <div className="col-sm-4 main-bold">Manaus</div>
+            </div>
+            <div className="row">
+              <div className="col-sm-1 main-bold">{BL.min}°</div>
+              <div className="col-sm-1 main-bold">{BL.min}°</div>
+              <div className="col-sm-4 main-bold">Belém</div>
+              <div className="col-sm-1 main-bold">{JP.min}°</div>
+              <div className="col-sm-1 main-bold">{JP.max}°</div>
+              <div className="col-sm-4 main-bold">João Pessoa</div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- weather/src/services/api.js
+++ weather/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const API_KEY = "85221c32c6777cb7cb5a3885aa3f3c47";
+const API_KEY = "a110dc149dab4574995abc097292d7d2";
 
 export const getCityWeather = cityName => {
   return axios.get(
-    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&lang=pt`
+    `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName},BR&key=${API_KEY}&lang=pt`
   );
 };
